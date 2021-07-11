4cbe45c591652c8c8c321ee30b4d20fb4f82bdb0
MJ <mohamed.aref32@gmail.com>
2021-07-10 21:47:59 -0500

    added photos for different models and started the header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Header from './components/Header';
+import Header from './components/Header';
 import Home from "./components/Home";
  
 import './App.css';
@@ -7,7 +7,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <Header/> */}
+      <Header/>
       <Home/>
     </div>
   );
