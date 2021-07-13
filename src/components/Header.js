d064ae8cd1445a1c81bb54b691231aa3098a6194
MJ <mohamed.aref32@gmail.com>
2021-07-13 15:21:49 -0500

    styled the header and added menuicon from material UI added media screen adjustments for small size screen and responsive design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from "styled-components";
+import MenuIcon from '@material-ui/icons/Menu';
 
 
 function Header() {
@@ -9,11 +10,16 @@ function Header() {
          <img src="images/logo.svg" alt="tesla-logo" />
          </a>
          <Menu>
-            <p><a href="#">Model s</a></p>
-            <p><a href="#">Model 3</a></p>
-            <p><a href="#">Model X</a></p>
-            <p><a href="#">Model Y</a></p>
+            <a href="#">Model s</a>
+            <a href="#">Model 3</a>
+            <a href="#">Model X</a>
+            <a href="#">Model Y</a>
          </Menu>
+         <RightMenu>
+            <a href="#"> Shop</a>
+            <a href="#"> Tesla Account</a>
+            <CostumMenu/>
+         </RightMenu>
       
       </Container>
    )
@@ -28,6 +34,7 @@ min-height: 60px;
 position: fixed;
 display:flex;
 align-items: center;
+justify-content: space-between;
 padding: 0 20px;
 top: 0;
 left: 0;
@@ -40,13 +47,33 @@ align-items: center;
 justify-content: center;
 flex: 1;
 
-p{
+a{
   font-weight:600;
   text-transform: uppercase;
   padding: 0 10px;
   flex-wrap: no-wrap;
 }
 
+@media (max-width:768px){
+   display: none;
+}
+`;
+
+const RightMenu = styled.div`
+display: flex;
+align-items: center;
  
+a{
+  font-weight:600;
+  text-transform: uppercase;
+  margin-right: 10px;
+   
+}
+`;
+
+const CostumMenu= styled(MenuIcon)`
+cursor:pointer;
+
 
 `;
+
